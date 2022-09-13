4f15b6d2d0590871810f775d2dac25fc45a9ed60
Logan Barnett <logan@alchemycodelab.com>
2022-09-13 11:16:15 -0700

    initial commit
    This includes a working candies and cats routes. It's not a complete
    solution and other things in the router must be considered.

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from 'node:http';
+import dotenv from 'dotenv';
+import app from './lib/app.js';
+
+dotenv.config();
+
+const server = http.createServer(app);
+
+const port = process.env.APP_PORT || 8080;
+const hostname = process.env.APP_HOST || 'localhost';
+
+server.listen(port, hostname, () => {
+    console.log(`Server listening on ${JSON.stringify(server.address())}!`);
+});
+console.log('Starting to listen...');
